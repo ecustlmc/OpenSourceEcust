32fe18cecf79bac44a71e919d4183a69c582a519
ecust-lmc <lmc1998@qq.com>
2019-12-18 15:42:27 +0800

    报告标题、摘要、正文放在一个字段里

--- app_report/models.py
+++ app_report/models.py
@@ -5,8 +5,9 @@ class UserProfile(models.Model):
     user = models.ForeignKey(User,verbose_name="用户",on_delete=models.CASCADE)
     name = models.CharField(max_length=50,verbose_name="姓名")
 
-class Message(models.Model):
+class Report(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     time = models.DateTimeField(verbose_name="发表时间")
-    title = models.CharField(max_length=100,verbose_name="标题")
-    text = models.TextField(verbose_name="正文")
+    #title = models.CharField(max_length=100,verbose_name="标题")
+    content = models.TextField(verbose_name="正文")
+
